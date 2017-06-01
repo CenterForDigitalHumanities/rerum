b16c9c66775bdc232d7ebb9783fae0196177836d
cubap <cubap@slu.edu>
2017-06-01 15:37:01 -0500

    oops.
    forgot to add .rerum.io to the address

--- app/utils.js
+++ app/utils.js
@@ -427,10 +427,10 @@ angular.module('utils', [])
                 updating = true;
             }
             if(isRerum && updating){ //It is a RERUM object for updating
-                url = "http://api/updateAnnotation.action?content=";
+                url = "http://api.rerum.io/updateAnnotation.action?content=";
             }
             else if(!updating){ //It is an object meant to be saved
-                url = "http://api/saveNewAnnotation.action?content=";
+                url = "http://api.rerum.io/saveNewAnnotation.action?content=";
             }
             var obj_str = JSON.stringify(obj); //Serialize JSON data into a string.
             url += obj_str;
@@ -439,7 +439,7 @@ angular.module('utils', [])
                 if(conf){
                     delete obj['@id']; //get rid of key:val, we do not want to preserve it.
                     obj_str = JSON.stringify(obj);
-                    url = "http://api/saveNewAnnotation.action?content=" + obj_str; //It is now a domestic manifest
+                    url = "http://api.rerum.io/saveNewAnnotation.action?content=" + obj_str; //It is now a domestic manifest
                 }
                 else{
                     return false;
