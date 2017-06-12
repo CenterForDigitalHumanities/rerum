aac1df98475a675e9faced5eeada8df1280ef52b
Bryan Haberberger <bhaberbe@slu.edu>
2017-06-12 10:43:35 -0500

    OMG Java Source Packages

---
+++ Source Packages/java_api/Java_Constants.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_api;
+
+/**
+ *
+ * @author bhaberbe
+ */
+public class Java_Constants {
+     public static String ANNOTATION_SERVER_ADDR = "http://165.134.241.141/annotationstore";
+}
